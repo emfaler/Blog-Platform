a31e574eb46c9706932c4097e16f2f5882fe79cd
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-03-06 16:43:31 -0500

    added migration

--- BlogPlatform/Models/Content.cs
+++ BlogPlatform/Models/Content.cs
@@ -12,7 +12,7 @@ namespace blog_template_practice.Models
         public string Body { get; set; }
         public string Author { get; set; }
         public string PublishDate { get; set; }
-        public Category Category { get; set; }
+        public virtual Category Category { get; set; }
 
        
 
